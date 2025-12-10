4d36d08adcf1014a7f67a37cbad298c94002f4ce
小懒 <76729550+xiaolan1314@users.noreply.github.com>
2025-12-10 10:30:28 +0800

    Update install.sh
    新增加速镜像 https://github.xloard.com/

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 功能: NodeScriptKit 安装和更新脚本
 
+
+
 goos=$(uname -s | tr '[:upper:]' '[:lower:]')
 goarch=$(uname -m)                       
 
@@ -27,10 +29,13 @@ else
     exit 1
 fi
 
-BIN_VERSION="$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/NodeSeekDev/NskCore/releases/latest)"
+#加速镜像
+PROXY_URL= "https://github.xloard.com/"
+
+BIN_VERSION="$(curl -Ls -o /dev/null -w %{url_effective} ${DOWNLOAD_URL}/https://github.com/NodeSeekDev/NskCore/releases/latest)"
 BIN_VERSION=${BIN_VERSION##*/}
 BIN_FILENAME="nskCore-$goos-$arch$ext"
-BIN_URL="https://github.com/NodeSeekDev/NskCore/releases/download/$BIN_VERSION/$BIN_FILENAME"
+BIN_URL="${DOWNLOAD_URL}/https://github.com/NodeSeekDev/NskCore/releases/download/$BIN_VERSION/$BIN_FILENAME"
 
 curl -Lso /usr/bin/nskCore $BIN_URL
 chmod u+x /usr/bin/nskCore
@@ -41,7 +46,7 @@ if tar --version 2>&1 | grep -qi 'busybox'; then
     fi
 fi
 
-MENU_URL="$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/NodeSeekDev/NodeScriptKit/releases/latest)"
+MENU_URL="$(curl -Ls -o /dev/null -w %{url_effective} ${DOWNLOAD_URL}/https://github.com/NodeSeekDev/NodeScriptKit/releases/latest)"
 MENU_VERSION="${MENU_URL##*/}"
 
 mkdir -p /etc/nsk/modules.d/default
@@ -49,7 +54,7 @@ mkdir -p /etc/nsk/modules.d/extend
 
 cd /tmp
 temp_dir=$(mktemp -d)
-curl -sLo - $temp_download_file "https://github.com/NodeSeekDev/NodeScriptKit/archive/refs/tags/$MENU_VERSION.tar.gz" | \
+curl -sLo - $temp_download_file "${DOWNLOAD_URL}/https://github.com/NodeSeekDev/NodeScriptKit/archive/refs/tags/$MENU_VERSION.tar.gz" | \
     tar -xzv -C $temp_dir
 [ -f "/etc/nsk/config.toml" ] || cp $temp_dir/*/menu.toml /etc/nsk/config.toml
 rm -rf /etc/nsk/modules.d/default/*  # Remove old scripts to prevent conflicts
